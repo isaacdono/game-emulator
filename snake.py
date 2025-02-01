31911e6679cad11ea86f1765880fb74814c2738e
Isaac Oliveira <isaac052503@gmail.com>
2025-02-01 12:44:02 -0300

    fix: xy pins are now correct and joystick works properly

--- snake.py
+++ snake.py
@@ -8,25 +8,24 @@ i2c = SoftI2C(scl=Pin(15), sda=Pin(14))
 # Initialize OLED Display (128x64)
 oled = SSD1306_I2C(128, 64, i2c)
 
-# Test: Display "Snake Game"
-oled.fill(0)  # Clear screen
-oled.text("Snake Game", 10, 30)
-oled.show()
-
-time.sleep(2)
-oled.fill(0)
-oled.show()
-
-# ---------------------------------------------------------
-
 # Initialize Joystick (ADC)
-x_axis = ADC(Pin(26))  # GP26 -> X-axis
-y_axis = ADC(Pin(27))  # GP27 -> Y-axis
+x_axis = ADC(Pin(27))  # GP27 -> X-axis
+y_axis = ADC(Pin(26))  # GP26 -> Y-axis
+
+# Test Direction
+THRESHOLD = 3000  # Sensitivity range
 
 while True:
-    x_val = x_axis.read_u16()  # Read X (0-65535)
-    y_val = y_axis.read_u16()  # Read Y (0-65535)
+    x_val = x_axis.read_u16()
+    y_val = y_axis.read_u16()
 
-    print("X:", x_val, " Y:", y_val)  # Print values
-    time.sleep(0.2)  # Small delay
+    if y_val > 60000:  # Up
+        print("UP")
+    elif y_val < 5000:  # Down
+        print("DOWN")
+    elif x_val > 60000:  # Right
+        print("RIGHT")
+    elif x_val < 5000:  # Left
+        print("LEFT")
 
+    time.sleep(0.5)  # Small delay
